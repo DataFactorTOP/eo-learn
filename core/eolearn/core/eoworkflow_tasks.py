b18de9b1e21173a680336f301da1975efb2aa8c1
maleksandrov <matej.aleksandrov@sinergise.com>
2021-06-10 17:06:32 +0200

    minor style update

--- core/eolearn/core/eoworkflow_tasks.py
+++ core/eolearn/core/eoworkflow_tasks.py
@@ -1,15 +1,15 @@
 """
-Module implementing tasks that have a special effect in EOWorkflow
+Module implementing tasks that have a special effect in `EOWorkflow`
 """
 from .eotask import EOTask
 
 
 class OutputTask(EOTask):
-    """ Stores EOPatch as an output of EOWorkflow results
+    """ Stores `EOPatch` as an output of `EOWorkflow` results
     """
     def __init__(self, name=None, features=...):
         """
-        :param name: A name under which EOPatch will be saved in ``WorkflowResults``
+        :param name: A name under which `EOPatch` will be saved in `WorkflowResults`
         :type name: str or None
         :param features: A collection of features to be kept
         :type features: an object supported by the :class:`FeatureParser<eolearn.core.utilities.FeatureParser>`
@@ -19,7 +19,7 @@ class OutputTask(EOTask):
 
     @property
     def name(self):
-        """ Provides a name under which output EOPatch will be saved
+        """ Provides a name under which output `EOPatch` will be saved
 
         :return: A name
         :rtype: str
@@ -28,7 +28,7 @@ class OutputTask(EOTask):
 
     def execute(self, eopatch):
         """
-        :param eopatch: input EOPatch
+        :param eopatch: input `EOPatch`
         :type eopatch: EOPatch
         :return: A shallow copy of the given `EOPatch` which will be stored as a result of a workflow
         :rtype: EOPatch
